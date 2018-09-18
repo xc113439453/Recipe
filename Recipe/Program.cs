7c6451aa9c9962d6aa29d84d8b38f3fec364f7d0
xuechi <113439453@qq.com>
2018-09-18 16:22:49 +0800

    多线程学习

--- Recipe/Program.cs
+++ Recipe/Program.cs
@@ -2,14 +2,93 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using static System.Console;
 
-namespace Recipe
+namespace Recipe01
 {
-    class Program
+
+    abstract class CounterBase
     {
-        static void Main(string[] args)
+        public abstract void Increment();
+        public abstract void Decrement();
+    }
+
+    class Counter : CounterBase
+    {
+        private int count;
+
+        public int Count => count;
+
+        public override void Decrement()
         {
+            count--;
+        }
+
+        public override void Increment()
+        {
+            count++;
         }
     }
+    class CounterNoLock : CounterBase
+    {
+        private int count;
+
+        public int Count => count;
+        public override void Decrement()
+        {
+            Interlocked.Decrement(ref count);
+        }
+
+        public override void Increment()
+        {
+            Interlocked.Increment(ref count);
+        }
+    }
+    class Program
+    {
+        static void TestCounter(CounterBase c)
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                c.Increment();
+                c.Decrement();
+            }
+        }
+        static void Main(string[] args)
+        {
+            WriteLine("Incorrect counter");
+            var c1 = new Counter();
+
+            var t1 = new Thread(() => TestCounter(c1));
+            var t2 = new Thread(() => TestCounter(c1));
+            var t3 = new Thread(() => TestCounter(c1));
+            t1.Start();
+            t2.Start();
+            t3.Start();
+            t1.Join();
+            t2.Join();
+            t3.Join();
+            WriteLine($"Total count: {c1.Count}");
+            WriteLine("--------------------------");
+
+            WriteLine("Correct counter");
+            var c2 = new CounterNoLock();
+
+            t1 = new Thread(() => TestCounter(c2));
+            t2 = new Thread(() => TestCounter(c2));
+            t3 = new Thread(() => TestCounter(c2));
+            t1.Start();
+            t2.Start();
+            t3.Start();
+            t1.Join();
+            t2.Join();
+            t3.Join();
+
+            WriteLine($"Total count: {c2.Count}");
+        }
+    }
+
 }
+
---
+++ Recipe02/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Recipe02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string MutexName = "Multithreading";
+            using (var m = new Mutex(false, MutexName))
+            {
+                // WaitOne方法的作用是阻止当前线程，直到收到其他实例释放的处理信号。
+                // 第一个参数是等待超时时间，第二个是否退出上下文同步域。
+                if (!m.WaitOne(TimeSpan.FromSeconds(10), false))
+                {
+                    WriteLine("Second instance is running!");
+                    ReadLine();
+                }
+                else
+                {
+                    WriteLine("Running!");
+                    ReadLine();
+                    // 释放互斥资源
+                    m.ReleaseMutex();
+                }
+            }
+            ReadLine();
+        }
+    }
+}
